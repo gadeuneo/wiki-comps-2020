b3fd944ae2ba52e3fe4a9fee7043dce6e35f29bb
James Gardner <8356171+gadeuneo@users.noreply.github.com>
2020-01-22 16:46:48 -0600

    sample bot login

--- api_project.py
+++ api_project.py
@@ -34,25 +34,48 @@ def printJsonTree(d, indent=0):
             print(": " + str(type(d[key])).split("'")[1] + " - " + str(len(str(d[key]))))
 
 url = "https://en.wikipedia.org/w/api.php?"
+
+# url = "https://www.mediawiki.org/w/api.php?"
+
 headers = {
-    "User-Agent": "BotCarletonComps2020/0.5 (http://www.cs.carleton.edu/cs_comps/1920/wikipedia/index.php) Python/3.6.9 Requests/2.18.14",
-    "Connection": "close"
+    "User-Agent": "BotCarletonComps2020/0.5 (http://www.cs.carleton.edu/cs_comps/1920/wikipedia/index.php) Python/3.6.9 Requests/2.18.14"
+    # "Connection": "close"
 }
 title = "2019–20_Hong_Kong_protests"
 # https://stackoverflow.com/questions/7136343/wikipedia-api-how-to-get-the-number-of-revisions-of-a-page
-params = { 'action': 'query',
-           'format': 'json',
-           'continue': '',
-           'titles': title,
-           'prop': 'revisions',
-           'rvprop': 'ids',
-           'rvlimit': 'max'}
 
-username = ""
-password = ""
+getToken = {
+    "action": "query",
+    "meta": "tokens",
+    "type": "login",
+    "format": "json"
+}
+
 credentials = ""
-
 with open("credentials.txt", "r", encoding="utf-8") as f:
     credentials = f.read()
 
-print(credentials)
+credentials = credentials.split()
+username = credentials[0]
+password = credentials[1]
+
+
+S = rq.Session()
+
+botLogin = S.get(url=url, params=getToken).json()
+
+token = botLogin['query']['tokens']['logintoken']
+
+login = {
+    "action": "login",
+    "lgname": username,
+    "lgpassword": password,
+    "lgtoken": token,
+    "format": "json"
+}
+
+botLogin = S.post(url=url, data=login).json()
+
+printJsonTree(botLogin)
+
+# print(botLogin['login']['result'])
