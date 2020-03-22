1a045ada7eee7d2cb8c8606deac89c5a42c4bd25
Jackie Chan <jackiec1998@gmail.com>
2020-03-22 17:57:07 -0500

    Revised the documentation for the helper functions and README.md highlighting the codebase.

--- README.md
+++ README.md
@@ -1,95 +1,31 @@
-# wiki-comps-2020
-Carleton College CS Comps
+# wiki-comps-2020 Project Description
+Carleton College Computer Science Comps
 
-Advisor: Sneha Narayan
+__Advisor:__ Sneha Narayan
 
-Students: Jackie Chan, James Gardner, Junyi Min, Kirby Mitchell
+__Students:__ Jackie Chan, James Gardner, Junyi Min, Kirby Mitchell
 
-Source: http://www.cs.carleton.edu/cs_comps/1920/wikipedia/index.php
+__Source:__ http://www.cs.carleton.edu/cs_comps/1920/wikipedia/index.php
 
-Paper: https://doi.org/10.1145/2998181.2998232
+__Paper:__ https://doi.org/10.1145/2998181.2998232
 
-Project Task Board: https://github.com/gadeuneo/wiki-comps-2020/projects/1
+__Project Task Board:__ https://github.com/gadeuneo/wiki-comps-2020/projects/1
 
-## api_project.py
-Contains code for collecting redirects, revisions, pageids, and pageviews via MediaWiki API. Accesses the API using a Wiki bot user account with login credentials stored in "credentials.txt" which is excluded via gitignore.
+# Codebase Overview
 
-Broken into sections separated by docstring quotes denoting what each section is. The sections are: helper functions, bot login, data collection functions, and data collection.
+### `titles.txt`
+Contains all the Wikipedia articles in our analysis.
+
+### `main.py`
+Contains all the code that pulls the relevant, necessary data for analysis. This includes: `creation/creation_dates.csv`, `10 Year Redirect Data/*`, and `10 Year Revision Data/*`. If data needs to be updated, delete the relevant file and rerun the relevant helper function to pull that data. These *user-facing* functions include: `generate_creation_dates_data()`, `generate_revision_data()`, and `generate_redirect_data()`.
+
+*Note: `main.py` access MediaWiki API using a Wiki bot user account with login credentials stored in `credentials.txt` which is excluded via `.gitignore`.*
+
+### `wiki_notebook.ipynb`
+Contains the code that generates the `10year.csv` file that concatenates all the .csv files in `10 Year Revision Data`.
+
+### `stats.py`
+Contains code for calculating various metrics such as Jaccard Similarity or correlations between pages and creating plots.
 
 ### Bot Login
 Allows authenciated requests to the MediaWiki API for less restricted access. Follows guidelines here: https://www.mediawiki.org/wiki/API:Login
-
-### Data Collection Functions
-
-#### getRevisions
-Input: pageID, startDate, endDate (epoch times)
-
-Output: list of dictionaries containing revisions between date range. Returns timestamp, editor, editor id, revision id, and size in bytes of edit.
-
-#### getPageviews
-Input: pageID
-
-Output: list of dictionaires containing date and pageview count.
-
-#### getRedirects
-Input: pageID, startDate, endDate (epoch times)
-
-Output: all redirects to the page within the time range as a list of dictionaries.
-
-#### getCreationDate
-Input: pageID
-
-Output: date of page creation
-
-### Static Helper functions
-
-#### get_credentials
-Input: None
-
-Output: the username and password, in that order, from the `credentials.txt` file created for the bot.
-
-#### get_titles
-Input: None
-
-Output: a list of titles, that are strings, of the titles listed in the `titles.txt` file from the repository.
-
-#### create_directories
-Input: the names of the directories to be created.
-
-Output: None, but generates the directories requested by the directory names passed in.
-
-#### add_talk_pages
-Input: should take in the created list of titles from `get_titles()`.
-
-Output: appends the string `"Talk:"` to each of the items in the list.
-
-#### format_file_names()
-Input: a list of names, it should be the titles.
-
-Output: a list of names, but with spaces replaced with underscores, periods replaced with `"(dot)"`, and colons replaced with `"(colon)"`.
-
-#### file_exists
-Input: given a complete file name.
-
-Output: True or False depending on whether the file exists.
-
-#### printJsonTree:
-Input: dictionary that is the JSON object.
-
-Output: Pretty-prints a JSON/Python dictionary recursively showing dictionary keys and their corresponding values in byte-size. Mainly a helper function for debugging purposes.
-
-#### printQueryErrors:
-Input: a request object.
-
-Output: Pretty-prints JSON/Python dictionary result from the MediaWiki API. Will print to the terminal if there are any errors, or print a success message if there are no errors. Mainly a helper function for debugging purposes.
-
-#### hasError:
-Input: a request object.
-
-Output: returns True or False based on the MediaWiki API results. A helper and safety function to ensure queries do not crash the script.
-
-### Data Collection
-Contains code to collect data on a list of Wiki page titles and saves the results in csv files using pandas. Stores files in a folder called "data" and the names of the files are the titles with modifications to allow for valid filenames. Also gets Talk page information by adding "Talk:" to the front of each page title.
-
-## stats.py
-Contains code for calculating various metrics such as Jaccard Similarity or correlations between pages and creating plots.
--- static_helpers.py
+++ static_helpers.py
@@ -4,6 +4,14 @@ import sys
 import time
 from datetime import datetime
 
+'''
+    Description: Reads the first two lines in credentials.txt which should be
+    the username and password of the MediaWiki API bot.
+    
+    Input: Nothing.
+    
+    Output: Two strings, username and password.
+'''
 def get_credentials():
 
     with open("credentials.txt", "r", encoding="utf-8") as file:
@@ -15,6 +23,14 @@ def get_credentials():
 
     return username, password
 
+'''
+    Description: Reads the titles in titles.txt and returns a list of titles in
+    the file. Omits any empty strings.
+
+    Input: Nothing.
+
+    Output: A list of titles from titles.txt.
+'''
 def get_titles():
 
     with open("titles.txt", "r", encoding="utf-8") as file:
@@ -28,32 +44,62 @@ def get_titles():
 
     return titles
 
-# Pass in a list of directories to create
+'''
+    Description: Creates directories for the given list of directory names if
+    they do not exist.
+
+    Input: A list of strings of directory names.
+
+    Output: Directories in the root directory.
+'''
 def create_directories(directory_names):
 
     for directory_name in directory_names:
-        if (not os.path.exists(directory_name)):
-            os.mkdir(directory_name)
+        create_directory(directory_name)
 
     return
 
+'''
+    Description: Creates the directory with the given name/string if it does not
+    exist already.
+
+    Input: A string for the directory name.
+
+    Output: A directory with the given name if it does not exist.
+'''
 def create_directory(directory_name):
     if (not os.path.exists(directory_name)):
         os.mkdir(directory_name)
     
     return
 
-# Given a list of titles, returns a copy of the list with talk pages.
-# Doesn't change the original list given.
+'''
+    Description: Given a list of titles, appends the string "Talk:" to the
+    beginning of it. Does NOT modify the given list.
+
+    Input: A list of strings.
+
+    Output: A copy of the list with the additional titles with "Talk:" appended
+    to the front.
+'''
 def add_talk_pages(titles):
 
     titles_copy = titles.copy()
 
-    for i in range(len(titles)):
-        titles_copy.append("Talk:" + titles[i])
+    for title in titles_copy:
+        titles_copy.append("Talk:" + title)
 
     return titles_copy
 
+'''
+    Descripton: Given a string, replaces spaces with underscores, periods with
+    "(dot)", and colons with "(colon)". With the modified string, it then
+    appends ".csv" to the title.
+
+    Input: A string/title.
+
+    Output: A modified string based on the rules in the description.
+'''
 def format_file_names(title):
 
     rules = {
@@ -67,6 +113,14 @@ def format_file_names(title):
 
     return title + ".csv"
 
+'''
+    Description: Given a complete path to a particular file, returns True or
+    False based on if it exists.
+
+    Input: A complete path to a particular file.
+
+    Output: True or False based on if the file exists.
+'''
 def file_exists(complete_file_name):
 
     if os.path.isfile(complete_file_name):
@@ -75,20 +129,15 @@ def file_exists(complete_file_name):
     return False
 
 '''
-    Begin Helper Functions
+    Description: Prints the different levels of nested keys in a JSON object.
+    When there are no more dictionaries to key into, prints objects type and
+    byte-size. Also iterates through lists of dictionaries.
+
+    Input: A dictionary representing a JSON object.
+
+    Output: Prints to console.
 '''
-
 def printJsonTree(d, indent=0):
-    """Print tree of keys in JSON object.
-
-    Prints the different levels of nested keys in a JSON object. When there
-    are no more dictionaries to key into, prints objects type and byte-size.
-    Also iterates through lists of dictionaries.
-
-    Input
-    -----
-    d : dict
-    """
     for key,value in d.items():
         print("\t"*indent + str(key),end=" ")
         if isinstance(value, dict):
@@ -102,6 +151,15 @@ def printJsonTree(d, indent=0):
         else:
             print(": " + str(type(d[key])).split("'")[1] + " - " + str(len(str(d[key]))))
 
+'''
+    Description: Pretty-prints JSON/Python dictionary result from the MediaWiki
+    API. Will print to the terminal if there are any errors, or print a success
+    message if there are no errors.
+
+    Input: A request object.
+
+    Output: Nothing, it prints to the terminal.
+'''
 def printQueryErrors(requestObject):
     warn = False
     error = False
@@ -143,6 +201,15 @@ def printQueryErrors(requestObject):
         print("*"*80)
         print("QUERY SUCESSFUL!")
 
+'''
+    Description: Checks for a given request object if there were any warning or
+    errors contained in it. A helper function to ensure queries do not crash
+    the script.
+
+    Input: A request object.
+
+    Output: True or False based on the containment of warnings or errors.
+'''
 def hasError(requestObject):
     warn = False
     error = False
@@ -163,6 +230,15 @@ def hasError(requestObject):
     else:
         return False
 
+'''
+    Description: Initializes the start and end dates for analysis when formatted
+    in "YYYY-MM-DD". Enforces some sanity checks as well and returns the
+    formatted dates.
+
+    Input: Two dates formatted in "YYYY-MM-DD".
+
+    Output: Correctly formatted time for analysis.
+'''
 def format_time(start_date, end_date):
     # Convert date to Unix Timestamp
     start_date = int(time.mktime(datetime.strptime(start_date, "%Y-%m-%d").timetuple()))
@@ -175,7 +251,15 @@ def format_time(start_date, end_date):
 
     return start_date, end_date
 
-def get_page_id(S, url, headers, title):
+'''
+    Description: Given a title, makes a request to the MediaWiki API to get the
+    page ID.
+
+    Input: The current session, url, headers, and the title of the article.
+
+    Output: The page ID for the article.
+'''
+def get_page_id(session, url, headers, title):
     page = {
             "action": "query",
             "prop": "revisions",
@@ -183,12 +267,12 @@ def get_page_id(S, url, headers, title):
             "format": "json"
         }
 
-    data = S.get(url=url, headers=headers, params=page).json()
+    data = session.get(url=url, headers=headers, params=page).json()
     if (hasError(data)):
         print("Query Error! Exiting program!")
         sys.exit(1)
     if (int(list(data['query']['pages'].keys())[0]) == -1):
-        print("TITLE: {0} NOT FOUND! PLEASE CHECK TITLE SPELLING!".format(title))
+        print("Title: {0} not found! Please check the spelling!".format(title))
         sys.exit(1)
     page_id = list(data['query']['pages'].keys())[0]
     return page_id
