e534f589ecfab57930dbf0565a3efe50ae004532
James Gardner <8356171+gadeuneo@users.noreply.github.com>
2020-04-15 18:36:17 -0500

    fix NaN corr issues

--- bug.py
+++ bug.py
@@ -1,187 +1,37 @@
-'''
-Gets metadata of wikipage per revision.
-Saves in csv format per revision.
-
-James Gardner
-'''
-
-import requests as rq
-import json
+import pandas as pd
 import os
 import sys
+import requests as rq
 from datetime import datetime as dt
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
 import time
-import pandas as pd
-
 # functions that are frequently accessed by other files
 from static_helpers import *
 
+path = "dailyPageviews"
 
-'''
-    Begin Bot Login Code.
-'''
-def login(S, url, headers):
-    url = url
-    headers = headers
-    getToken = {
-        "action": "query",
-        "meta": "tokens",
-        "type": "login",
-        "format": "json"
-    }
+titles = ["Death_of_Luo_Changqing.csv", "Talk(colon)Death_of_Luo_Changqing.csv"]
 
-    username, password = get_credentials()
+def reformatFiles():
 
-    botLogin = S.get(url=url, params=getToken).json()
+    for i in range(len(titles)):
+        pageDf = pd.read_csv(os.path.join(path, titles[i]))
+        # prints daily sums
+        # print(pageDf.sum(axis=0, skipna=True))
+        # prints page title sums
+        # print(pageDf.sum(axis=1, skipna=True))
+        pageviews = [["Date", "Count"]]
 
-    token = botLogin['query']['tokens']['logintoken']
-
-    login = {
-        "action": "login",
-        "lgname": username,
-        "lgpassword": password,
-        "lgtoken": token,
-        "format": "json"
-    }
-
-    botLogin = S.post(url=url, data=login).json()
-
-# printJsonTree(botLogin)
-
-# print(botLogin['login']['result'])
-
-'''
-    End Bot Login Code
-'''
-
-
-
-'''
-    Begin Data Collection Functions
-'''
-
-def get_revisions(S, url, headers, pageid, start=None, end=None):
-    if (start == None or end == None):
-        print("ERROR, need start and end date!")
-        sys.exit(1)
-    else:
-        revisions = {
-            "action": "query",
-            "prop": "revisions",
-            # "titles": title,
-            "pageids": pageid,
-            "rvprop": "timestamp|user|userid|ids|size",
-            "rvslots": "*",
-            "rvlimit": "max",
-            # "rvlimit": 5,
-            "rvstart": start,
-            "rvend": end,
-            "rvdir": "newer",
-            # "rvcontinue": "",
-            "format": "json",
-            "continue": "",
-            "formatversion": 2,
-            "maxlag": 5
-        }
-        done = False
-        allRevs = []
-
-        while (not done):
-            revs = S.get(url=url, headers=headers, params=revisions).json()
-            if (hasError(revs)):
-                print("Query Error! Exiting program!")
-                sys.exit(1)
-            # printJsonTree(revs)
-            revList = revs['query']['pages'][0]['revisions']
-            for rev in revList:
-                allRevs.append(rev)
-
-            if ("continue" in revs):
-                revisions['continue'] = revs['continue']['continue']
-                revisions['rvcontinue'] = revs['continue']['rvcontinue']
+        for col in pageDf.columns[1:]:
+            if (dt.strptime(col, "%Y-%m-%d") > dt.strptime("2019-12-10", "%Y-%m-%d")):
+                break
             else:
-                done = True
+                pageviews.append([col, pageDf[col].sum()])
 
-        return allRevs
-
-
-def create_revision_data(session, url, headers, titles, start_date, end_date,
-    debug_mode=False):
-
-    directory = "10 Year Revision Data"
-    create_directory(directory)
-
-    file_names = [title + ".csv" for title in titles]
-
-    for title, file_name in zip(titles, file_names):
-
-        try:
-            page_id = get_page_id(session, url, headers, title)
-            revision_data = get_revisions(session, url, headers, page_id,
-                start=start_date, end=end_date)
-
-            complete_path = os.path.join(directory, file_name)
-
-            df_revisions = pd.DataFrame(revision_data)
-
-            # Add page_id as a column in the dataframe.
-            df_revisions['page_id'] = page_id
-
-            if (not debug_mode):
-                if (not os.path.isfile(complete_path)):
-                    df_revisions.to_csv(complete_path, encoding="utf-8")
-                # else:
-                #     print("Did not overwrite {0} because it currently exists!"
-                #         .format(file_name))
-        except:
-            print("Data not found for {0}.".format(title))
-            print("Page ID: {0}".format(page_id))
-
-    return
-
-'''
-    End Data Collection Functions
-'''
-
-def main():
-
-    S = rq.Session()
-
-    url = "https://en.wikipedia.org/w/api.php?"
-
-    headers = {
-        "User-Agent": "BotCarletonComps2020/0.8 \
-            (http://www.cs.carleton.edu/cs_comps/1920/wikipedia/index.php) \
-            Python/3.6.9 Requests/2.18.14",
-        "connection": "keep-alive"
-        # "Connection": "close"
-    }
-
-    login(S, url, headers)
-
-    # To change included titles, go to titles.txt
-    titles = get_titles()
-    titles = add_talk_pages(titles)
-
-    start_date, end_date = format_time(start_date="2009-12-10",
-        end_date="2019-12-10")
-
-    # Use pageid for curid to check if correct page is found
-    # https://en.wikipedia.org/?curid=
+        dfData = pd.DataFrame(pageviews[1:], columns=pageviews[0])
+        dfData.to_csv(os.path.join(path, titles[i]), encoding="utf-8")
 
 
-    '''
-        BUG: Pages: "Death/Killing of Luo Changqing" and "List of March-June
-        2019 Hong Kong protests" were not found. This includes their talk pages.
-    '''
 
-    '''
-        BUG: Pages: "Civil Human Rights Front", "Hong Kong Way",  and "List of
-        {March-June, December} 2019 Hong Kong protests" were not found.
-    '''
-
-    create_revision_data(S, url, headers, titles,
-        start_date, end_date)
-
-if __name__ == "__main__":
-    main()
+reformatFiles()
