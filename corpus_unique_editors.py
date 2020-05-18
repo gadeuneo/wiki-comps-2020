8ee7ceead104ed3e47fc3f62f87fb46eaae87057
James Gardner <8356171+gadeuneo@users.noreply.github.com>
2020-05-18 16:35:50 -0500

    merge fixes to coprus editors

--- corpus_unique_editors.py
+++ corpus_unique_editors.py
@@ -3,7 +3,7 @@
     Plots monthly unique editors across corpus
 
 
-    Written by Junyi
+    Written by Junyi.  Updated by James Gardner to work with new data folders.
 '''
 
 import pandas as pd
@@ -21,64 +21,15 @@ register_matplotlib_converters()
 start = time.time()
 
 # folder of files
-path = "10years"
+path = "10 Year Revision Data"
 plotPath = "figures"
-
 directories = ["figures"]
-
 create_directories(directories)
 
-# working list of Wiki pages
-titles = get_titles()
-
-# adds talk pages
-titles = add_talk_pages(titles)
-
-# converts titles to filename format
-titles = [format_file_names(title) for title in titles]
-titleArray = []
-for title in titles:
-    titleArray.append("Data" + title)
-    titleArray.append("Redirects" + title)
-
-# check if file exists, if not, remove from list of titles
-for title in titles:
-    if (not os.path.isfile(os.path.join(path, "Data" + title))):
-        filename = "Data" + title
-        titleArray.remove(filename)
-
-    if (not (os.path.isfile(os.path.join(path, "Redirects" + title)))):
-        filename = "Redirects" + title
-        titleArray.remove(filename)
-
-
-##### TODO: merge files? -- Which ones? How?
-##### TODO: save merged files?
-
-
-dataDict = dict()
-
-
-for f in titleArray:
-    dataDict[f[:-4]] = pd.read_csv(os.path.join(path, f))
-
-# print(dataDict.keys())
-
-allData = []
-allRed = []
-for key in dataDict.keys():
-    if ("Data" in key):
-        allData.append(dataDict[key])
-    else:
-        allRed.append(dataDict[key])
-
-revisionData = pd.concat(allData, ignore_index=True, sort=False)
-revisionData['timestamp'] = pd.to_datetime(revisionData['timestamp'])
-# TODO: double check inplace param
-revisionData.sort_values(by='timestamp', inplace = True)
-revisionData['timestamp'] = revisionData['timestamp'].astype(str)
-revisionData['timestamp'] = revisionData['timestamp'].str.replace(" ", "T").str[:-6] + "Z"
-#print(revisionData.to_string())
+revisonFiles = os.listdir(path)
+revisionDict = dict()
+for r in revisonFiles:
+    revisionDict[r[:-4]] = pd.read_csv(os.path.join(path, r))
 
 # Convert date to Unix Timestamp
 startDate = int(time.mktime(dt.strptime("2009-12-10", "%Y-%m-%d").timetuple()))
@@ -87,12 +38,6 @@ today = int(time.mktime(dt.today().timetuple()))
 # Assertions for proper date args
 assert(startDate <= endDate)
 assert(endDate <= today)
-
-#separate out "DATA-" articles from "REVISION-", without the .csv
-dataTitleArray = []
-for title in titleArray:
-    if title[0:4] == "Data":
-        dataTitleArray.append(title[:-4])
 
 #Plots monthly unique editors over time
 def plotMonthly():
@@ -174,8 +119,8 @@ def plotAbsoluteOverTimeGraph():
 #Returns the data in the form of [date - set of editors on that date]
 def returnData():
     editorDict = dict()
-    for title in dataTitleArray:
-        article = dataDict[title]
+    for title in revisionDict.keys():
+        article = revisionDict[title]
         for index, rowData in article.iterrows():
             editDate = dt.strptime(rowData['timestamp'], "%Y-%m-%dT%H:%M:%SZ").date()
             if(rowData['userid'] != 0): #not anon
